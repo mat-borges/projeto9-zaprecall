c4230aff07fbfc2f57b69dbbffd09bfe93407b9e
Mateus Borges <borgesm.work@gmail.com>
2022-09-29 18:08:01 -0300

    tweaks 2.0 (recomponentização DeckFooter->ShowText)

--- src/components/Deck/DeckFooter/DeckFooter.js
+++ src/components/Deck/DeckFooter/DeckFooter.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
-import sad from '../../../assets/img/sad.png';
-import happy from '../../../assets/img/party.png';
 import ShowIcons from './ShowIcons.js';
+import ShowText from './ShowText';
 
 export default function DeckFooter(props) {
 	const { deck, cardsDone, goal, zappedCards, iconState } = props;
@@ -16,48 +15,6 @@ export default function DeckFooter(props) {
 			</div>
 		</Footer>
 	);
-}
-
-function ShowText(props) {
-	const { cardsDone, goal, zappedCards, deck } = props;
-
-	if (cardsDone === deck.length) {
-		if (zappedCards >= goal) {
-			return (
-				<Texto>
-					<div>
-						{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}
-						/{goal}
-					</div>
-					<div>
-						Parabéns, você cumpriu sua meta!
-						<img src={happy} alt="happy" />
-					</div>
-				</Texto>
-			);
-		} else {
-			return (
-				<Texto>
-					<div>
-						{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}
-						/{goal}
-					</div>
-					<div>
-						Putz!
-						<img src={sad} alt="sad" />
-					</div>
-				</Texto>
-			);
-		}
-	} else {
-		return (
-			<Texto>
-				{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}/
-				{goal}
-				<img src={zappedCards >= goal ? happy : ''} alt="" />
-			</Texto>
-		);
-	}
 }
 
 const Footer = styled.div`
@@ -79,17 +36,4 @@ const Footer = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	gap: 20px;
-`;
-
-const Texto = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	div {
-		flex-direction: column;
-	}
-	img {
-		margin-left: 5px;
-	}
 `;
---
+++ src/components/Deck/DeckFooter/ShowText.js
@@ -0,0 +1,58 @@
+import sad from '../../../assets/img/sad.png';
+import happy from '../../../assets/img/party.png';
+import styled from 'styled-components';
+
+export default function ShowText(props) {
+	const { cardsDone, goal, zappedCards, deck } = props;
+
+	if (cardsDone === deck.length) {
+		if (zappedCards >= goal) {
+			return (
+				<Texto>
+					<div>
+						{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}
+						/{goal}
+					</div>
+					<div>
+						Parabéns, você cumpriu sua meta!
+						<img src={happy} alt="happy" />
+					</div>
+				</Texto>
+			);
+		} else {
+			return (
+				<Texto>
+					<div>
+						{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}
+						/{goal}
+					</div>
+					<div>
+						Putz!
+						<img src={sad} alt="sad" />
+					</div>
+				</Texto>
+			);
+		}
+	} else {
+		return (
+			<Texto>
+				{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}/
+				{goal}
+				<img src={zappedCards >= goal ? happy : ''} alt="" />
+			</Texto>
+		);
+	}
+}
+
+const Texto = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	div {
+		flex-direction: column;
+	}
+	img {
+		margin-left: 5px;
+	}
+`;
