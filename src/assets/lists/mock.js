5799538275279154b945524d457935f1e6856311
Mateus Borges <borgesm.work@gmail.com>
2022-09-29 18:05:15 -0300

    tweaks

--- src/assets/lists/mock.js
+++ src/assets/lists/mock.js
@@ -1,55 +1,36 @@
-const INITIALCOLOR =
-	'invert(0%) sepia(98%) saturate(7%) hue-rotate(162deg) brightness(103%) contrast(102%)';
-
 const recallJS = [
 	{
 		question: 'O que é JSX?',
 		answer: 'Uma extensão de linguagem do JavaScript',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'O React é __',
 		answer: 'uma biblioteca JavaScript para construção de interfaces',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Componentes devem iniciar com __',
 		answer: 'letra maiúscula',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Podemos colocar __ dentro do JSX',
 		answer: 'expressões',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'O ReactDOM nos ajuda __',
 		answer: 'interagindo com a DOM para colocar componentes React na mesma',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Usamos o npm para __',
 		answer: 'gerenciar os pacotes necessários e suas dependências',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Usamos props para __',
 		answer: 'passar diferentes informações para componentes',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Usamos estado (state) para __',
 		answer:
 			'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente',
-		status: '',
-		color: INITIALCOLOR,
 	},
 ];
 
@@ -57,50 +38,34 @@ const recallRandom = [
 	{
 		question: 'Biscoito ou bolacha?',
 		answer: 'Bolacha... obviamente',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Começar a coxinha pela parte maior ou menor?',
 		answer: 'De novo, menor, obviamente...',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Toddy ou nescau?',
 		answer: 'Toddy!',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'BK ou Mc',
 		answer: 'BK pela batata',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Ketchup na pizza?',
 		answer: 'NÃO, porém de vez em quando sim',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Batata frita com sorvete?',
 		answer: 'Tem gosto pra tudo na vida',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Acabaram as ideias',
 		answer: 'Boa sorte',
-		status: '',
-		color: INITIALCOLOR,
 	},
 	{
 		question: 'Ainda sem ideias',
 		answer: 'Fim!',
-		status: '',
-		color: INITIALCOLOR,
 	},
 ];
 
--- src/components/Deck/DeckFooter/DeckFooter.js
+++ src/components/Deck/DeckFooter/DeckFooter.js
@@ -6,37 +6,9 @@ import ShowIcons from './ShowIcons.js';
 export default function DeckFooter(props) {
 	const { deck, cardsDone, goal, zappedCards, iconState } = props;
 
-	function renderFooter() {
-		if (cardsDone === deck.length) {
-			if (zappedCards >= goal) {
-				return (
-					<p>
-						Parabéns, você cumpriu sua meta!
-						<img src={happy} alt="happy" />
-					</p>
-				);
-			} else {
-				return (
-					<p>
-						Putz!
-						<img src={sad} alt="sad" />
-					</p>
-				);
-			}
-		} else {
-			return (
-				<p>
-					{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}/
-					{goal}
-					<img src={zappedCards >= goal ? happy : ''} alt="" />
-				</p>
-			);
-		}
-	}
-
 	return (
 		<Footer>
-			{renderFooter()}
+			<ShowText cardsDone={cardsDone} goal={goal} zappedCards={zappedCards} deck={deck} />
 			<div>
 				{deck.map((e, i) => (
 					<ShowIcons key={i} i={i} iconState={iconState} />
@@ -44,6 +16,48 @@ export default function DeckFooter(props) {
 			</div>
 		</Footer>
 	);
+}
+
+function ShowText(props) {
+	const { cardsDone, goal, zappedCards, deck } = props;
+
+	if (cardsDone === deck.length) {
+		if (zappedCards >= goal) {
+			return (
+				<Texto>
+					<div>
+						{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}
+						/{goal}
+					</div>
+					<div>
+						Parabéns, você cumpriu sua meta!
+						<img src={happy} alt="happy" />
+					</div>
+				</Texto>
+			);
+		} else {
+			return (
+				<Texto>
+					<div>
+						{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}
+						/{goal}
+					</div>
+					<div>
+						Putz!
+						<img src={sad} alt="sad" />
+					</div>
+				</Texto>
+			);
+		}
+	} else {
+		return (
+			<Texto>
+				{cardsDone}/{deck.length} Concluidos || Meta: {zappedCards >= goal ? goal : zappedCards}/
+				{goal}
+				<img src={zappedCards >= goal ? happy : ''} alt="" />
+			</Texto>
+		);
+	}
 }
 
 const Footer = styled.div`
@@ -55,6 +69,7 @@ const Footer = styled.div`
 	z-index: 1;
 	width: 100%;
 	min-height: 70px;
+	max-height: 90px;
 	padding: 14px 10px;
 	font-size: 18px;
 	color: black;
@@ -64,9 +79,17 @@ const Footer = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	gap: 20px;
+`;
+
+const Texto = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	div {
-		display: flex;
-		justify-content: center;
-		align-items: center;
+		flex-direction: column;
+	}
+	img {
+		margin-left: 5px;
 	}
 `;
